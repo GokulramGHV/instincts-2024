0cf35fe5b73cadcdf9bb04bfdf87db3e24afa6a3
vaishnave3 <vaishnavejp3@gmail.com>
2024-01-31 23:53:46 +0530

    changed png to svg

--- src/sections/landing/about_us.jsx
+++ src/sections/landing/about_us.jsx
@@ -11,20 +11,20 @@ export default function AboutUs() {
           <div className="my-auto mx-auto md:mx-0 z-10">
             <img src="/about_us/blimp.png" />
           </div>
-          <div className="hidden md:block -ml-2 z-0">
+          <div className="hidden md:block -ml-3 z-0 md:-ml-5 lg:w-64 xl:w-60 2xl:w-40">
             <div className="h-1/2">
-              <img src="/about_us/top.png" className="h-full" />
+              <img src="/about_us/top.svg" className="h-full float-bottom" />
             </div>
             <div className="h-1/2">
-              <img src="/about_us/bottom.png" className="h-full" />
+              <img src="/about_us/bottom.svg" className="h-full" />
             </div>
           </div>
           <div className="md:hidden flex -mt-3 -ml-1.5 z-0">
-            <div className="w-[40%] md:w-1/2 -rotate-[6.5deg] z-10">
-              <img src="/about_us/top.png" className="w-full h-full" />
+            <div className="md:w-1/2 -rotate-[6.5deg] z-10">
+              <img src="/about_us/top.svg" className="w-full h-full" />
             </div>
-            <div className="w-[60%] md:w-1/2">
-              <img src="/about_us/bottom.png" className="w-full h-full" />
+            <div className="md:w-1/2">
+              <img src="/about_us/bottom.svg" className="w-full h-full" />
             </div>
           </div>
           <div className="bg-white md:w-[40%] p-5 md:mx-0 mx-auto z-20 lg:text-base text-xs">
