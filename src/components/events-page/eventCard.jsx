9b334ef79ff4e7729e85bd4da07321ee1eb074ef
BlitzJB <blitz04.dev@gmail.com>
2024-02-08 09:52:22 +0530

    feat: events card mobile responsive

--- src/components/events-page/eventCard.jsx
+++ src/components/events-page/eventCard.jsx
@@ -12,11 +12,13 @@ export default function EventCard({ eventImage, eventColor, eventTitle, eventDay
         setIsBeingHovered(false)
     }
     
-    return <div style={{ borderColor: isBeingHovered ? eventColor : "black" }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="lg:w-[calc(25%-20px)] md:w-[calc(33%-20px)] sm:w-[calc(45%-20px)] w-full transition-colors flex flex-col md:lg:w-[calc(33.33%-20px)] min-h-80 bg-[#F2F2F2] rounded-xl box-border mr-5 mb-12 md:flex-wrap border-4 border-black">
-        <img className="w-full p-5" src={eventImage} alt="" />
-        <div className="p-5">
-            <h1 className="text-2xl font-bold">{eventTitle}</h1>
-            <p className="text-lg">{eventDayDetail}</p>
+    return <div style={{ borderColor: isBeingHovered ? eventColor : "black" }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="lg:w-[calc(25%-20px)] md:w-[calc(33%-20px)] sm:w-[calc(45%-20px)] w-full transition-colors flex flex-col md:lg:w-[calc(33.33%-20px)] sm:min-h-80 bg-[#F2F2F2] rounded-xl box-border mr-5 mb-12 md:flex-wrap border-4 border-black">
+        <div className="flex sm:flex-col flex-row">
+            <img className="sm:w-full p-5 sm:aspect-auto aspect-square sm:h-auto h-32 w-h-32" src={eventImage} alt="" />
+            <div className="p-5 sm:pl-5 pl-0">
+                <h1 className="text-2xl font-bold">{eventTitle}</h1>
+                <p className="text-lg">{eventDayDetail}</p>
+            </div>
         </div>
         <p style={{ backgroundColor: eventColor }} className="text-lg mt-auto rounded-b-lg text-right font-semibold text-white px-2">{eventCategory}</p>
     </div>
