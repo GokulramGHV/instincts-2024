6120dfbd9098d77a427968ad31787ecc5782e449
Vetrivel <vetrisurabi@gmail.com>
2024-02-13 17:10:47 +0530

    non responsive coming soon page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,14 +2,11 @@ import AboutUs from '@/sections/landing/about_us';
 import Footer from '@/sections/footer/footer';
 import GallerySection from '@/sections/landing/gallery-section';
 import HeroSection from '@/sections/landing/hero-section';
-
+import ComingSoon from '@/sections/coming_soon/coming-soon';
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <HeroSection />
-      <AboutUs />
-      <GallerySection/>
-      <Footer />
+      <ComingSoon />
     </main>
   );
 }
