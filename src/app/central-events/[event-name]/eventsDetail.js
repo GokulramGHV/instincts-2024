a3843fb845e4439b22cef276f09f24769572d5fd
Vishal <vismbs@pm.me>
2024-02-22 09:29:14 +0530

    feat: added images

--- src/app/central-events/[event-name]/eventsDetail.js
+++ src/app/central-events/[event-name]/eventsDetail.js
@@ -3,6 +3,9 @@ export const eventDetails = {
         "name": "CHORENITE",
         "description": "At Instincts 24, the pulsating heartbeat of creativity resonates through the corridors of our campus as students converge for a whirlwind of cultural celebration. Among the many electrifying events, Choreonite stands as a dazzling spectacle, where the stage becomes a canvas for artistic expression. Here, students from different colleges and universities unite, each step a brushstroke painting the air with their passion for dance. With expert judges on the lookout for the next dance sensation, every twist and turn becomes a testament to skill and artistry. Yet beyond the competition lies the essence of unity and celebration, where dance transcends barriers, forging bonds that echo long after the final curtain falls. Instincts 24 embodies the spirit of creativity, fostering a vibrant tapestry of culture and expression that illuminates the college landscape. So come, and join us at Choreonite, where the spotlight awaits and dreams take flight on the wings of dance!",
         "images": [
+            "/central_events/event-images/choreonite/1.JPG",
+            "/central_events/event-images/choreonite/2.jpg",
+            "/central_events/event-images/choreonite/3.jpg"
         ]
     },
     "womens-day": {
@@ -21,19 +24,27 @@ export const eventDetails = {
         "name": "PROSHOW",
         "description": "Get ready for the most thrilling night of INSTINCTS'24! On Day 2, we raise her hands and dance as the sun sets for our Pro Show. Celebrities and singers turning the stage into a dazzling playground of entertainment. It's like your favorite playlist came to life! The Pro Show isn't just any gig, it's a superstar cast making magic happen right before your eyes. This show brings all the feels, from foot-tapping beats to heartwarming melodies. It's like a giant musical hug that'll leave you buzzing with good vibes. So don't forget to keep a lookout for the upcoming celebrity lineup for it!",
         "images": [
+            "/central_events/event-images/proshow/1.JPG",
+            "/central_events/event-images/proshow/2.JPG",
+            "/central_events/event-images/proshow/3.jpg"
         ]
     },
     "celeb-show": {
         "name": "CELEBRITY VARIETY SHOW",
         "description": "Get ready to laugh until your sides ache at Instincts 24's exclusive Celebrity Variety Show! This year, we're putting the spotlight on stand-up comedy, bringing you an evening filled with uproarious laughter and side-splitting humor. In addition to the uproarious stand-up comedy performances, the Celebrity Variety Show at Instincts 24 offers an evening of pure entertainment and camaraderie. As the lights dim and the stage comes alive, audience members are treated to an array of comedic styles and personalities, each bringing their  unique flair to the spotlight. The Celebrity Variety Show promises an unforgettable experience that celebrates the universal language of laughter. So come prepared to let loose, unwind, and embrace the joyous spirit of comedy at this unforgettable event!",
         "images": [
+            "/central_events/event-images/celeb-show/1.JPG",
+            "/central_events/event-images/celeb-show/2.JPG",
+            "/central_events/event-images/celeb-show/3.JPG"
         ]
     },
     "fire-silambam": {
         "name": "FIRE SILAMBAM",
         "description": "Prepare to be mesmerized by the intense energy of our fire silambam show at Instincts'24! As flames dance in sync with the movements of our skilled performers, the atmosphere ignites with artistry and adrenaline. Witness the ancient martial art of silambam come alive, captivating the audience with its blend of tradition and contemporary flair. Let the fiery spectacle at Instincts leave you spellbound, as each spin and twirl of the flaming staffs paints a vivid picture for you. Join us for an unforgettable experience that will ignite your senses and leave you craving for more!",
         "images": [
-
+            "/central_events/event-images/fire_silambam/3.jpg",
+            "/central_events/event-images/fire_silambam/2.jpg",
+            "/central_events/event-images/fire_silambam/1.jpg"
         ]
     },
     "djnite": {
