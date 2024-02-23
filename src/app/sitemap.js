a557ab470d7bfe18ac2209fb0a97dc5473c96ec9
R Akash <akashrangesh07@gmail.com>
2024-02-23 13:53:19 +0530

    added sitemap.xml and robots.txt

---
+++ src/app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://ssnsnucinstincts.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.ssnsnucinstincts.com/events',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.ssnsnucinstincts.com/central-events',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.ssnsnucinstincts.com/contact-us',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.ssnsnucinstincts.com/sponsors',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.ssnsnucinstincts.com/get-passes',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ];
+}
