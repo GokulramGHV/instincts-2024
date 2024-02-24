ecd528ac0ff5e469623a12dc359f8fe5502b33cf
Gagan <rollingrocky360@gmail.com>
2024-02-24 22:00:26 +0530

    fix: about-us blank on scroll, overflow on >=2xl screens

--- src/sections/landing/the-team.jsx
+++ src/sections/landing/the-team.jsx
@@ -7,7 +7,7 @@ const londrina = Londrina_Solid({
 
 const president = { title: 'President, SSN Institutions and Pro Chancellor, SNU Chennai', name: 'Dr. Kala Vijaykumar' }
 const vc = { title: 'Vice Chancellor, SNU Chennai', name: 'Dr. S. K. Bhattacharyya' }
-const princi = { title: 'Principal, SNU Institutions', name: 'Dr. V. E. Annamalai' }
+const princi = { title: 'Principal, SSN Institutions', name: 'Dr. V. E. Annamalai' }
 const facultyConvenor = { title: 'Faculty Convenor', name: 'Dr. Sunita Nair' }
 const deptCoords = { title: 'Department Coordinators',  names: [
     'Hakash D',
@@ -35,27 +35,27 @@ const jointSecretaries = { title: 'Joint Secretaries', names: ['Deepak M', 'Madh
 function NameCard({ details }) {
   return (
     <div style={{width: details.title.length > 30 ? '45%' : 'auto'}}>
-      <h2 className={`${londrina.className} text-base md:text-2xl xl:text-3xl 2xl:text-5xl w-full leading-tight mb-1 xl:mb-3`}>{details.title}</h2>
+      <h2 className={`${londrina.className} text-base md:text-2xl xl:text-3xl 2xl:text-4xl w-full leading-tight mb-1 xl:mb-3`}>{details.title}</h2>
       {details.names ? 
-      <ul className='flex flex-col list-none items-center font-satoshi text-sm md:text-xl xl:text-2xl 2xl:text-4xl'>
+      <ul className='flex flex-col list-none items-center font-satoshi text-sm md:text-xl xl:text-2xl 2xl:text-3xl'>
         {details.names.map(name => <li className='xl:mb-1' key={name}>{name}</li>)}
       </ul> : 
-      <p className='font-satoshi text-sm md:text-xl xl:text-2xl 2xl:text-4xl'>{details.name}</p>}
+      <p className='font-satoshi text-sm md:text-xl xl:text-2xl 2xl:text-3xl'>{details.name}</p>}
     </div>
   )
 }
 
 export default function TheTeam() {
   return (
-    <section className="w-full bg-[#E6FCFF] py-8 lg:py-12">
-      <div className="w-full flex items-center mb-8 md:mb-12 justify-center 2xl:justify-between overflow-clip gap-1 sm:gap-4">
-        <img src='/the_team/saopaulo.svg' className="h-[5rem] md:h-[8rem] lg:h-[10rem] xl:h-[12rem] 2xl:h-[18rem]" />
-        <h1 className={`text-4xl md:text-6xl xl:text-8xl 2xl:text-9xl min-w-fit ${londrina.className}`}>THE TEAM</h1>
-        <img src='/the_team/saopaulo.svg' className="h-[5rem] md:h-[8rem] rotate-180 lg:h-[10rem] xl:h-[12rem] 2xl:h-[18rem]" />
+    <section className="w-full bg-[#E6FCFF] py-8 lg:py-12 xl:py-16">
+      <div className="w-full flex items-center mb-8 md:mb-12 justify-center overflow-clip gap-1 sm:gap-4">
+        <img src='/the_team/saopaulo.svg' className="h-[5rem] md:h-[8rem] lg:h-[10rem] xl:h-[12rem] mr-auto" />
+        <h1 className={`text-4xl md:text-6xl xl:text-8xl min-w-fit xl:mx-4 ${londrina.className}`}>THE TEAM</h1>
+        <img src='/the_team/saopaulo.svg' className="h-[5rem] md:h-[8rem] rotate-180 lg:h-[10rem] xl:h-[12rem] ml-auto" />
       </div>
-      <div className='flex flex-col my-4 items-center text-center gap-10 md:gap-14 2xl:gap-24 lg:w-[70%] m-auto'>
+      <div className='flex flex-col my-4 items-center text-center gap-10 md:gap-14 2xl:gap-20 lg:w-[70%] m-auto'>
         <NameCard details={president} />
-        <div className='flex gap-6 md:gap-24 2xl:gap-32'>
+        <div className='flex gap-6 md:gap-24 2xl:gap-28'>
           <NameCard details={vc} />
           <NameCard details={princi} />
         </div>
@@ -66,7 +66,7 @@ export default function TheTeam() {
           <NameCard details={treasurers} />
           <NameCard details={docHeads} />
         </div>
-        <div className='flex gap-6 md:gap-24 2xl:gap-32'>
+        <div className='flex gap-6 md:gap-24 2xl:gap-28'>
           <NameCard details={secretaries} />
           <NameCard details={jointSecretaries} />
         </div>
