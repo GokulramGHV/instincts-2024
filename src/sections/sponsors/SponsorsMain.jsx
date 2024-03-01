4dbd684cb8adbf20c69e99c7d6ec539101aa2de5
Gagan <rollingrocky360@gmail.com>
2024-03-01 11:05:07 +0530

    add: partnership headline

--- src/sections/sponsors/SponsorsMain.jsx
+++ src/sections/sponsors/SponsorsMain.jsx
@@ -6,32 +6,78 @@ const londonFont = Londrina_Solid({
 });
 
 const companies = [
-  { name: 'unstop', website: 'https://unstop.com/', width: 474, height: 188 },
+  { 
+    name: 'unstop', 
+    website: 'https://unstop.com/', 
+    backgroundClip: 1,
+    width: 474, 
+    height: 188,
+  },
   {
     name: 'sattva',
     website: 'https://www.sattva.in/',
+    backgroundClip: 2,
     width: 297,
     height: 64,
   },
   {
     name: 'ags_cinemas',
     website: 'https://www.agscinemas.com/',
+    backgroundClip: 3,
     width: 220,
     height: 136,
   },
-  {
-    name: 'tnie',
-    website: 'https://www.newindianexpress.com/',
-    width: 400,
-    height: 161,
-  },
-  {
-    name: 'accord',
-    website: 'https://www.theaccordhotels.com/',
-    width: 300,
-    height: 167,
-  },
 ];
+
+const accord = {
+  name: 'accord',
+  website: 'https://www.theaccordhotels.com/',
+  backgroundClip: 5,
+  width: 300,
+  height: 167,
+}
+const tnie = {
+  name: 'tnie',
+  website: 'https://www.newindianexpress.com/',
+  backgroundClip: 4,
+  width: 400,
+  height: 161,
+}
+
+const hcl = {
+  name: 'hcl',
+  website: "https://www.hcltech.com/",
+  backgroundClip: 3,
+  width: 418,
+  height: 221,
+}
+
+function SponsorCard({company, heightStyle="h-[7rem] md:h-[11rem]", widthStyle='w-1/2'}={}) {
+  return (
+    <div>
+      <a
+        key={company.name}
+        href={company.website}
+        target="_blank"
+        className={"aspect-[2] mx-auto flex items-center justify-center " + heightStyle}
+        style={{
+          backgroundImage: `url(/sponsors/clip${company.backgroundClip}.svg)`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
+        >
+        <Image
+          src={`/sponsors/${company.name}_logo.png`}
+          alt={company.name + ' logo'}
+          className={widthStyle}
+          width={company.width}
+          height={company.height}
+          />
+      </a>
+    </div>
+  )
+}
 
 export default function SponsorsMain() {
   return (
@@ -46,55 +92,29 @@ export default function SponsorsMain() {
         >
           Title Sponsor
         </h2>
-        <a
-          href="https://www.hcltech.com/"
-          target="_blank"
-          className="flex items-center justify-center aspect-[2] 
-            h-[9rem] md:h-[14rem] 2xl:h-[16rem]"
-          style={{
-            backgroundImage: 'url(/sponsors/clip3.svg)',
-            backgroundSize: 'contain',
-            backgroundRepeat: 'no-repeat',
-          }}
-        >
-          <Image
-            src="/sponsors/hcl_logo.png"
-            alt="hcl logo"
-            className="w-full"
-            width={418}
-            height={221}
-          />
-        </a>
+        <SponsorCard company={hcl} heightStyle='h-[9rem] md:h-[14rem] 2xl:h-[16rem]' widthStyle='w-[80%]'/>
       </div>
       <div className="w-full mt-20 lg:mt-28 2xl:mt-32">
         <h2
-          className={`${londonFont.className} text-[#FFFCEA] text-center text-4xl md:text-7xl lg:text-8xl pb-8 lg:pb-16`}
-        >
+          className={`${londonFont.className} text-[#FFFCEA] text-center text-4xl md:text-7xl pb-8 lg:pb-16`}>
           Other Sponsors
         </h2>
-        <div className="flex flex-wrap flex-col w-[95%] mx-auto gap-8 md:gap-y-16 md:flex-row justify-center">
-          {companies.map((company, index) => (
-            <a
-              key={company.name}
-              href={company.website}
-              target="_blank"
-              className="aspect-[2] h-[7rem] md:h-[11rem] 2xl:h-[18rem] flex items-center justify-center"
-              style={{
-                backgroundImage: `url(/sponsors/clip${index + 1}.svg)`,
-                backgroundSize: 'contain',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-              }}
-            >
-              <Image
-                src={`/sponsors/${company.name}_logo.png`}
-                alt={company.name + ' logo'}
-                className='w-[30%] sm:w-1/2'
-                width={company.width}
-                height={company.height}
-              />
-            </a>
-          ))}
+        <div className="flex flex-wrap flex-col mx-auto gap-8 md:gap-y-16 md:flex-row justify-center">
+          {companies.map(company => <SponsorCard company={company} key={company.name}/>)}
+        </div>
+      </div>
+      <div className='w-[95%] mx-auto mt-20 lg:mt-28 2xl:mt-32 flex justify-center gap-16 sm:gap-24 flex-wrap px-4'>
+        <div className='flex items-center flex-col'>
+          <h2 className={`${londonFont.className} text-[#FFFCEA] text-center text-3xl md:text-5xl pb-8`}>
+              Accomodation Partner
+          </h2>
+          <SponsorCard company={accord} />
+        </div>
+        <div className='flex items-center flex-col'>
+          <h2 className={`${londonFont.className} text-[#FFFCEA] text-center text-3xl md:text-5xl pb-8`}>
+            Print Partner
+          </h2>
+          <SponsorCard company={tnie} />
         </div>
       </div>
     </section>
