be0dc57f334c3a3c8610e4503917eda55d0eb254
thenithinbalaji <thenithinbalaji@gmail.com>
2024-03-04 23:20:40 +0530

    changed message below button in /getpasses

--- src/app/get-passes/page.jsx
+++ src/app/get-passes/page.jsx
@@ -18,29 +18,19 @@ export default function GetPassesPage() {
         <div className="flex flex-col justify-center items-center mt-10">
           <div className="flex gap-3 sm:gap-5 items-center">
 
-            <div className="flex flex-col items-center">
-              <div className="w-7 h-7 sm:w-10 sm:h-10 flex justify-center items-center border-[3px] border-[#A0A0A0] text-[#A0A0A0] rounded-full sm:text-2xl font-bold">
-                1
-              </div>
-              <div className="border-l-[3px] border-dashed h-6 sm:h-12 border-[#A0A0A0]"></div>
-              <div className="w-7 h-7 sm:w-10 sm:h-10 flex justify-center items-center border-[3px] border-[#A0A0A0] text-[#A0A0A0] rounded-full sm:text-2xl font-bold">
-                2
-              </div>
-            </div>
-
             <div className="flex flex-col gap-4 sm:gap-6">
               <Link
                 href="/events"
                 target="_blank"
-                className="rounded-full bg-[#0C5AB9] border border-[#2E3192] text-white whitespace-nowrap text-sm sm:text-xl 2xl:text-2xl font-[900] px-16 sm:px-20 2x:px-28 h-12 sm:h-14 2xl:h-16 flex justify-center items-center hover:shadow-xl transition-all duration-200 hover:opacity-90 w-fit"
+                className="rounded-full bg-[#0C5AB9] border border-[#2E3192] text-white whitespace-nowrap text-sm sm:text-xl 2xl:text-2xl font-[900] px-16 sm:px-20 2x:px-28 h-12 sm:h-14 2xl:h-16 flex justify-center items-center hover:shadow-xl transition-all duration-200 hover:opacity-90 w-fit mx-auto"
               >
                 VIEW EVENTS
               </Link>
 
               <div
-                className={`${londrinaSolid.className} my-5 text-black whitespace-nowrap text-lg sm:text-2xl 2xl:text-3xl font-[400] opacity-85 flex justify-center items-center sm:block`}
+                className={`${londrinaSolid.className} text-black whitespace-nowrap text-lg sm:text-2xl opacity-85 flex justify-center items-center sm:block`}
               >
-                Tap REGISTER in the Pop-up
+                Open the event you want to register for and click register
               </div>
             </div>
           </div>
